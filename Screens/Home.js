e36b07680b1f93106878d8c5a5d340712f30bda9
LinnLattOo <linnlattoo@gusto.edu.mm>
2022-11-03 01:58:09 +0630

    Home Page Navigation

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,17 +1,21 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import {useTheme} from '@react-navigation/native';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome5';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Products from './Products';
+import InputForm from './InputForm';
+import SystemNavigationBar from 'react-native-system-navigation-bar';
+SystemNavigationBar.navigationHide();
 const Tab = createBottomTabNavigator();
 const screenOptions = (route, color) => {
   let iconName;
   switch (route.name) {
     case 'Products':
-      iconName = 'file';
+      iconName = 'store-alt';
       break;
-    case 'SignIn':
-      iconName = 'home';
+    case 'Stock':
+      iconName = 'cubes';
       break;
     default:
       break;
@@ -20,28 +24,35 @@ const screenOptions = (route, color) => {
 };
 
 const RootNavigator = () => {
+  const {colors} = useTheme();
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Products"
-        screenOptions={({route}) => ({
-          tabBarActiveTintColor: '#e91e63',
-          tabBarInactiveTintColor: '#4f0e63',
-          tabBarIcon: ({color}) => screenOptions(route, color),
-        })}>
-        <Tab.Screen name="Products" component={Products} />
-        <Tab.Screen name="SignIn" component={SignIn} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator
+      initialRouteName="Products"
+      screenOptions={({route}) => ({
+        tabBarActiveTintColor: '#e91e63',
+        tabBarInactiveTintColor: colors.primary,
+        tabBarStyle: {
+          height: 65,
+          paddingVertical: 10,
+          paddingBottom: 10,
+        },
+        tabBarIcon: ({color}) => screenOptions(route, color),
+      })}>
+      <Tab.Screen
+        name="Products"
+        component={Products}
+        options={{
+          title: 'Products List',
+          tabBarLabel: 'Products',
+        }}
+      />
+      <Tab.Screen name="Stock" component={InputForm} />
+    </Tab.Navigator>
   );
 };
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+  return <RootNavigator />;
 };
 
 export default Home;
